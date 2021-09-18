463b87d6353638593663f97b3f13b2afa6628d3c
Pedda Babu Asapu <peddababuasapu@gmail.com>
2021-09-18 19:48:27 +1000

    removed excess code and deleted recent migration

--- store/models.py
+++ store/models.py
@@ -60,12 +60,6 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
 
-    class Meta:
-        db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name','first_name'])
-        ]
-
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
