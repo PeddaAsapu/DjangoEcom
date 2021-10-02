b397a6d6c7a39a3ab7fe0429bff149925cf6336d
Pedda Babu Asapu <peddababuasapu@gmail.com>
2021-10-03 00:15:58 +1000

    Registered apps in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from django.urls import reverse
+from django.utils.html import format_html,urlencode
+from django.http.request import HttpRequest
 from . import models
 # Register your models here.
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 20
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self,product):
@@ -25,4 +33,31 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 20
 
 
-admin.site.register(models.Collection)
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'Customer']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url =( 
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id' : str(collection.id)
+            }))
+
+
+        return format_html('<a href={}>{}<a>',url,collection.products_count)
+        # collection.products_count
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
+
--- store/models.py
+++ store/models.py
@@ -71,7 +71,10 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
 
     def __str__(self):
-        return self.first_name 
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
     
     
 
@@ -90,12 +93,15 @@ class Order(models.Model):
     payment_status = models.CharField(max_length=1,choices = PAYMENT_STATUS_CHOICES,default = PAYMENT_STATUS_PENDING )
     Customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
 
+    
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,on_delete=models.PROTECT)
     product = models.ForeignKey(Product,on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
+    
 
 #Address
 #one to many relationship
